285ae444bbcae3b922b471c3a9a37bfcef31018d
長崎　健斗 <kento@nagasakikentonoMacBook-Pro.local>
2019-10-30 14:01:18 +0900

    yeah

--- js/works.js
+++ js/works.js
@@ -1,10 +1,22 @@
 var kentoWorks = [
-    {
-        name: "じゃんけんアプリ",
-        tag: "app",
-        img: "janken-app.png",
-        github: "githubへのリンク",
-        features: `<h4 class="heading-4">こだわったポイント</h4>
+  {
+    name: "YONDEチラシ",
+    tag: "web",
+    img: "yonde.png",
+    github: "githubへのリンク",
+    features: `<h4 class="heading-4">こだわったポイント</h4>
+        <p>・いかにQRを読みこんでもらえるか。その一点にしぼり、全体の配置・配色を決めました。</p>
+        <p>・実際にフィードバックがもらえたりして、とてもいい経験になった</p>
+
+        <h4 class="heading-4">一言</h4>
+        <p>・初のイラレプロジェクト。初の印刷物。</p>`
+  },
+  {
+    name: "じゃんけんアプリ",
+    tag: "app",
+    img: "janken-app.png",
+    github: "githubへのリンク",
+    features: `<h4 class="heading-4">こだわったポイント</h4>
         <p>・デザイン！習ったCSSを駆使して、モダンな見た目を心掛けました。。</p>
         <p>・アイコンフォントfontAwesomeを使ってみた。</p>
         <p>・jQueryを使うことで、画面の切り替えや合計点数の計算を実現した。</p>
@@ -12,13 +24,13 @@ var kentoWorks = [
 
         <h4 class="heading-4">一言</h4>
         <p>・初めてjQueryを本格的に使ってみたんで、アドバイスお願いしますっ。</p>`
-    },
-    {
-        name: "ポートフォリオ",
-        img: "portfolio.jpg",
-        tag: "web",
-        github: "githubへのリンク",
-        features: `<h4 class="heading-4">こだわったポイント</h4>
+  },
+  {
+    name: "ポートフォリオ",
+    img: "portfolio.jpg",
+    tag: "web",
+    github: "githubへのリンク",
+    features: `<h4 class="heading-4">こだわったポイント</h4>
         <p>・XDを始めって使ってデザイン</p>
         <p>・学校で習ってきたことを総動員してみた</p>
         <p>・ポートフォリオ部分の入力がとてつもなく大変なので、オブジェクトを使って管理するようにしたのが工夫ポイント。</p>
@@ -26,26 +38,26 @@ var kentoWorks = [
 
         <h4 class="heading-4">一言</h4>
         <p>・ぜひツイッターで絡んでくださいっ</p>`
-    },
-    {
-        name: "料理の具材メモ",
-        img: "first-vue.jpg",
-        tag: "web app",
-        github: "githubへのリンク",
-        features: `<h4 class="heading-4">こだわったポイント</h4>
+  },
+  {
+    name: "料理の具材メモ",
+    img: "first-vue.jpg",
+    tag: "web app",
+    github: "githubへのリンク",
+    features: `<h4 class="heading-4">こだわったポイント</h4>
         <p>・始めてVue.jsに触ってみた。</p>
         <p>・こちらは、Udemyの講座の模写。javascriptとはかなり書き方が違うから、難しく感じた。。。と同時に、色々と新しい事ができるようになる気がするっ</p>
         <p>・やりながら、シングルページアプリケーションの意味を理解する事ができた。</p>
     
         <h4 class="heading-4">一言</h4>
         <p>・Vue.jsに興味持ったので、今度は勉強会も参加してみよかなっ</p>`
-    },
-    {
-        name: "おすすめの洋画・海ドラ紹介サイト",
-        img: "fav-shows.jpg",
-        tag: "web",
-        github: "githubへのリンク",
-        features: `<h4 class="heading-4">こだわったポイント</h4>
+  },
+  {
+    name: "おすすめの洋画・海ドラ紹介サイト",
+    img: "fav-shows.jpg",
+    tag: "web",
+    github: "githubへのリンク",
+    features: `<h4 class="heading-4">こだわったポイント</h4>
         <p>・ネットフリックス風のデザインを意識</p>
         <p>・APIを利用して、各ドラマの情報を引っ張ってきている</p>
         <p>・アニメーションの面で、バニラjavascript単体では厳しい面がいくつか。。。やはり、Vue.jsを早めに学んで行きたいなと感じるこの頃。</p>
@@ -53,13 +65,13 @@ var kentoWorks = [
 
         <h4 class="heading-4">一言</h4>
         <p>・リアルに面白い海外ドラマを紹介してるので、興味がある方はのぞいて下さいっ</p>`
-    },
-    {
-        name: "TOEIC英単語アプリ",
-        img: "toeic-app.png",
-        tag: "app",
-        github: "githubへのリンク",
-        features: `<h4 class="heading-4">こだわったポイント</h4>
+  },
+  {
+    name: "TOEIC英単語アプリ",
+    img: "toeic-app.png",
+    tag: "app",
+    github: "githubへのリンク",
+    features: `<h4 class="heading-4">こだわったポイント</h4>
         <p>・デザイン！習ったCSSを駆使して、モダンな見た目を心掛けました。。</p>
         <p>・単語の部分をフラッシュカードぽくCSSで作った。</p>
         <p>・Javascriptのみで複数画面の画面遷移を実装しています。</p>
@@ -67,22 +79,21 @@ var kentoWorks = [
 
         <h4 class="heading-4">一言</h4>
         <p>・単語を登録するのがかなりメンドくさいので、仕組みを作りたいなと感じた。</p>`
-    },
-    {
-        name: "日々Kento",
-        img: "blog.jpg",
-        tag: "web writing",
-        github: "githubへのリンク",
-        features: `<h4 class="heading-4">こだわったポイント</h4>
+  },
+  {
+    name: "日々Kento",
+    img: "blog.jpg",
+    tag: "web writing",
+    github: "githubへのリンク",
+    features: `<h4 class="heading-4">こだわったポイント</h4>
         <p>・僕のブログです！！！</p>
         <p>・気ままに更新してるのでぜひっ（笑</p>`
-    },
-    {
-        name: "Coming SOON",
-        tag: " ",
-        img: "comingsoon.jpg",
-        github: "githubへのリンク",
-        features: ``
-    },
-    
-]
+  },
+  {
+    name: "Coming SOON",
+    tag: " ",
+    img: "comingsoon.jpg",
+    github: "githubへのリンク",
+    features: ``
+  }
+];
